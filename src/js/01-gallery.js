6adda8d8d0da8334ef9d1728d19145dcd2308da5
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-01-02 18:33:52 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector('.gallery');
+
+const makeGalleryItemRef = ({ preview, original, description } = {}) =>
+  `
+    <li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+            data-source="${original}"
+        alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+
+const makeGallery = galleryItems.map(makeGalleryItemRef).join('');
+galleryListRef.insertAdjacentHTML('beforeend', makeGallery);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+(function createMarkup() {
+  const itemMarkup = galleryItems.map(templateFunction).join('');
+  galleryRefs.insertAdjacentHTML('beforeend', itemMarkup);
+})();
