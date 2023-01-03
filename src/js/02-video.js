76a5887a74b4267c818361bfbd34138445d609f8
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-01-03 21:25:21 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const key = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => localStorage.setItem(key, seconds), 1000)
+);
+
+player.setCurrentTime(localStorage.getItem(key));
